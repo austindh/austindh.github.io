29a2114988096de437da6973ae7f19611381ecb0
Austin Hughes <austindavidhughes@gmail.com>
2015-09-11 00:03:38 -0600

    CSS/HTML updates and added folder structure

--- Resume/compiled-templates.js
+++ Resume/compiled-templates.js
@@ -22,21 +22,32 @@ this["templates"]["resume"] = Handlebars.template({"1":function(depth0,helpers,p
     + this.escapeExpression(this.lambda(depth0, depth0))
     + "</li>\n";
 },"6":function(depth0,helpers,partials,data) {
-    var stack1, helper;
+    var stack1, helper, alias1=helpers.helperMissing, alias2=this.escapeExpression;
 
-  return "			<div class=\"language\">\n				<div class=\"name\">"
-    + this.escapeExpression(((helper = (helper = helpers.main || (depth0 != null ? depth0.main : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"main","hash":{},"data":data}) : helper)))
-    + "</div>\n"
+  return "			<div class=\"language "
+    + alias2((helpers.languageName || (depth0 && depth0.languageName) || alias1).call(depth0,(depth0 != null ? depth0.main : depth0),{"name":"languageName","hash":{},"data":data}))
+    + "\">\n				<div class=\"name\">"
+    + alias2(((helper = (helper = helpers.main || (depth0 != null ? depth0.main : depth0)) != null ? helper : alias1),(typeof helper === "function" ? helper.call(depth0,{"name":"main","hash":{},"data":data}) : helper)))
+    + "</div>\n<!--\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.related : depth0),{"name":"each","hash":{},"fn":this.program(7, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "			</div>\n";
+    + "-->\n			</div>\n";
 },"7":function(depth0,helpers,partials,data) {
     return "					<div class=\"relatedLanguage\">"
     + this.escapeExpression(this.lambda(depth0, depth0))
     + "</div>\n";
 },"9":function(depth0,helpers,partials,data) {
-    return "			<div class=\"application\">"
-    + this.escapeExpression(this.lambda(depth0, depth0))
-    + "</div>\n";
+    var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+
+  return "			<div class=\"course"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.languages : depth0),{"name":"each","hash":{},"fn":this.program(10, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "\">\n				<div class=\"courseNumber\">CS "
+    + alias3(((helper = (helper = helpers.number || (depth0 != null ? depth0.number : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"number","hash":{},"data":data}) : helper)))
+    + "</div>\n				<div class=\"courseTitle\">"
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</div>\n			</div>\n";
+},"10":function(depth0,helpers,partials,data) {
+    return " "
+    + this.escapeExpression((helpers.languageName || (depth0 && depth0.languageName) || helpers.helperMissing).call(depth0,depth0,{"name":"languageName","hash":{},"data":data}));
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
@@ -44,7 +55,8 @@ this["templates"]["resume"] = Handlebars.template({"1":function(depth0,helpers,p
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.jobs : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "	</div>\n</section>\n\n<section>\n	<h2>Skills</h2>\n\n	<h3>Languages and Frameworks</h3>\n	<div class=\"languageList\">\n"
     + ((stack1 = helpers.each.call(depth0,((stack1 = (depth0 != null ? depth0.skills : depth0)) != null ? stack1.languagesAndFrameworks : stack1),{"name":"each","hash":{},"fn":this.program(6, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "	</div>\n\n	<h3>Applications</h3>\n	<div class=\"applicationList\">\n"
-    + ((stack1 = helpers.each.call(depth0,((stack1 = (depth0 != null ? depth0.skills : depth0)) != null ? stack1.applications : stack1),{"name":"each","hash":{},"fn":this.program(9, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "	</div>\n\n	<h3>Computer Science Courses</h3>\n	<div class=\"courseList\">\n"
+    + ((stack1 = helpers.each.call(depth0,((stack1 = (depth0 != null ? depth0.skills : depth0)) != null ? stack1.csCoursesComplete : stack1),{"name":"each","hash":{},"fn":this.program(9, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers.each.call(depth0,((stack1 = (depth0 != null ? depth0.skills : depth0)) != null ? stack1.csCoursesCurrent : stack1),{"name":"each","hash":{},"fn":this.program(9, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "	</div>\n</section>\n\n<section>\n	<h2>Awards</h2>\n	<p>Thomas S. Monson Presidential Scholarship, Brigham Young University, August 2009</p>\n	<p>Eagle Scout, Boy Scouts of America, 2008</p>\n</section>\n<section>\n	<h2>Volunteer Work</h2>\n	<p>Missionary for the Church of Jesus Christ of Latter-day Saints in South Africa and Botswana, August 2010–August 2012</p>\n</section>\n\n\n\n";
 },"useData":true});
--- gulpfile.js
+++ gulpfile.js
@@ -11,30 +11,30 @@ gulp.task( 'default', function() {
 });
 
 gulp.task( 'templates', function() {
-	gulp.src( 'Templates/*.hbs' )
+	gulp.src( 'Resume/Templates/*.hbs' )
 		.pipe( handlebars() )
 		.pipe( wrap( 'Handlebars.template(<%= contents %>)' ) )
 		.pipe( declare({
 			  namespace: 'templates',
 			  noRedeclare: true, // Avoid duplicate declarations
 		}))
-		.pipe( concat( 'compiled-templates.js' ) )
+		.pipe( concat( 'Resume/compiled-templates.js' ) )
 		.pipe( gulp.dest( '' ) );
 		//.pipe( notify({ message: 'Templates compiled' }));
 });
 
 gulp.task( 'css', function() {
-	gulp.src( 'Sass files/*.scss' )
+	gulp.src( 'Resume/Sass files/*.scss' )
 		.pipe( sass().on( 'error', sass.logError ) )
 		.pipe( concat( 'site.css' ) )
 		.pipe( autoprefixer({
 			browsers: [ 'last 2 versions' ],
 			cascade: false
 		}) )
-		.pipe( gulp.dest( '' ) );
+		.pipe( gulp.dest( 'Resume/' ) );
 });
 
 gulp.task( 'watch', function() {
-	gulp.watch( 'Sass files/*.scss', [ 'css' ] );
-	gulp.watch( 'Templates/*.hbs', [ 'templates' ] );
+	gulp.watch( 'Resume/Sass files/*.scss', [ 'css' ] );
+	gulp.watch( 'Resume/Templates/*.hbs', [ 'templates' ] );
 });
