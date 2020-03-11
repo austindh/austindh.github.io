026c7f965fb8dd2a911a28b8fd3e31d50e2721fa
austin_hughes <austindavidhughes@gmail.com>
2020-03-10 23:35:00 -0600

    Basic about me page

--- app/src/components/AboutMe.tsx
+++ app/src/components/AboutMe.tsx
@@ -20,9 +20,28 @@ export const AboutMe = () => {
 	}, []);
 
 	return (
-		<div id="about-me" className="card">
-			<img src="/austin.jpg" alt="" className="pic"/>
-			<div className="pic"></div>
+		<div id="about-me">
+			<div className="contact card">
+				<img src="/austin.jpg" alt="" className="pic profile"/>
+				<div className="info">
+					<div>
+						<img className="icon" src="/email.svg" alt=""/>
+						<span>austindavidhughes@gmail.com</span>
+						{/* <span>mail@austinhughes.me</span> */}
+					</div>
+					<div>
+						<img className="icon" src="/github.png" alt=""/>
+						<span>austindh</span>
+					</div>
+					<div>
+						<img className="icon" src="/linkedin.svg" alt=""/>
+						<span>austindh816</span>	
+					</div>
+				</div>
+			</div>
+			<div className="card paragraph">
+				Here is some info
+			</div>
 		</div>
 	)
 };
